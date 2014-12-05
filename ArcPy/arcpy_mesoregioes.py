b8b2676125a816b936c6acc43dd63fcee25924c4
chamamur <caiohamamura@gmail.com>
2014-12-05 15:02:12 -0200

    Many scripts

--- ArcPy/arcpy_mesoregioes.py
+++ ArcPy/arcpy_mesoregioes.py
@@ -1,14 +1,10 @@
 import arcpy
 import time
 
-frags = arcpy.mapping.Layer('D:/Desktop/IPEF/28-05-2014/FragsWGS84.shp')
-mesoregion = arcpy.mapping.Layer('D:/Desktop/IPEF/28-05-2014/meso2005.shp')
+frags = arcpy.mapping.Layer('D:/Desktop/Caio/PROFISSIONAL/IPEF/28-05-2014/FragsWGS84.shp')
+mesoregion = arcpy.mapping.Layer('D:/Desktop/Caio/PROFISSIONAL/IPEF/28-05-2014/meso2005.shp')
 i=0
-for row in arcpy.da.SearchCursor(mesoregion, ['SHAPE@']):
-    if i<=1:
-        i+=1
-        continue
-    print i
-    result=arcpy.Clip_analysis(frags,row[0],'D:/Desktop/IPEF/28-05-2014/meso'+str(i)+'.shp')
+for row in arcpy.da.SearchCursor(mesoregion, ['SHAPE@','GEOCODIG4']):
+    result=arcpy.Clip_analysis(frags,row[0],'D:/Desktop/Caio/PROFISSIONAL/IPEF/28-05-2014/frags/frag_'+str(row[1])+'.shp')
     i+=1
     time.sleep(10)
